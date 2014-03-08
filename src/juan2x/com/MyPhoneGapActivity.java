7e7127519d8f814a3538d78232d1f73b492329f9
TwixD <juanDavidCRivera@hotmail.com>
2014-03-08 12:22:02 -0500

    Init proyect

---
+++ src/juan2x/com/MyPhoneGapActivity.java
@@ -0,0 +1,12 @@
+package juan2x.com;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
